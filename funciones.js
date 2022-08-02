447cc7fcb9fd89fa4829d9f20de5dc82c500d4a7
gustavo.rañieski <gustavoranieski@hotmail.com>
2022-08-02 15:37:30 -0300

    incluyendo arrays

--- funciones.js
+++ funciones.js
@@ -1,14 +1,45 @@
-function saludar() {
+//DESAFIO COMPLEMENTARIO-ARRAYS-
+class Producto {
+    constructor(nombre, precio, codigo) {
+    this.nombre = nombre.toUpperCase();
+    this.precio = parseFloat(precio);
+    this.codigo = codigo;
+    this.vendido = false;
+    }
+    sumaIva() {
+        this.precio = this.precio * 1.21;
+    }
+   }
+   
+   const productos = [];
+   productos.push(new Producto("banco niza", 11000,"m01"));
+   productos.push(new Producto("mesa londres", 15500,"m02"));
+   productos.push(new Producto("mueble york", 7850,"m03"));
+   productos.push(new Producto("mueble mini york", 5050,"m04"));
+   productos.push(new Producto("mesa zagreb", 15600,"m05"));
+   productos.push(new Producto("mesa panama", 6800,"m06"));
+   productos.push(new Producto("perchero gales", 6300,"m07"));
+   productos.push(new Producto("perchero lyon", 4050,"m08"));
+   
+   for(const prod of productos){
+     prod.sumaIva();
+    }
+    console.log(productos);
+
+
+//PRIMER DESAFIO-FUNCIONES-
+    function saludar() {
     alert("¡Bienvenidos a MUEBLITO!");
 }
 
 saludar ();
 
+
 function calcular()
 {
     let producto = prompt ("Ingresa un producto (presiona -t para ver el total)\nbanco niza\nmesa londres\nmueble york\nmueble mini york\nmesa zagreb\nmesa panama\nperchero gales\nperchero lyon");
     let precio=0;
-
+    
 while(producto!="t"){
     switch(producto){
         case "banco niza":
