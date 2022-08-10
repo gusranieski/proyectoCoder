afd2a4573e3d3d71b51b660173b3ecdb8866a8b4
gustavo.rañieski <gustavoranieski@hotmail.com>
2022-08-09 23:51:57 -0300

    incorporando dom

--- funciones.js
+++ funciones.js
@@ -1,10 +1,11 @@
 //-CONSTRUCTOR-
 class Producto {
-    constructor(nombre, precio, codigo, vendido) {
+    constructor(nombre, precio, codigo, stock, imagen) {
     this.nombre = nombre;
     this.precio = parseFloat(precio);
     this.codigo = codigo;
-    this.vendido = vendido;
+    this.stock = parseInt(stock);
+    this.imagen = imagen;
     }
     sumaIva() {
         this.precio = this.precio * 1.21;
@@ -12,19 +13,41 @@ class Producto {
 }
 
 const productos = [];
-productos.push(new Producto("banco niza", 11000,"m01", true));
-productos.push(new Producto("mesa londres", 15500,"m02", true));
-productos.push(new Producto("mueble york", 7850,"m03", false));
-productos.push(new Producto("mueble mini york", 5050,"m04",true));
-productos.push(new Producto("mesa zagreb", 15600,"m05", false));
-productos.push(new Producto("mesa panama", 6800,"m06", true));
-productos.push(new Producto("perchero gales", 6300,"m07", true));
-productos.push(new Producto("perchero lyon", 4050,"m08", true));
+productos.push(new Producto("banco niza",11000,"m01",10,"banco00"));
+productos.push(new Producto("mesa londres",15500,"m02",10,"escritorio00"));
+productos.push(new Producto("mueble york",7850,"m03",10,"estanteria00"));
+productos.push(new Producto("mueble mini york",5050,"m04",10,"estanteria01"));
+productos.push(new Producto("mesa zagreb",15600,"m05",10,"mesaratona00"));
+productos.push(new Producto("mesa panama",6800,"m06",10,"mesaratona01"));
+productos.push(new Producto("perchero gales",6300,"m07",10,"perchero00"));
+productos.push(new Producto("perchero lyon",4050,"m08",10,"perchero001"));
 //-(se muestran por consola agregándole el IVA)-
 for(const prod of productos){
     prod.sumaIva();
     }
     console.table(productos);
+
+//DOM-(se crean las cartas en el html)
+function crearTarjeta(){
+    let cartas = document.getElementById("cartas");
+    for(const prod of productos){
+        let carta = document.createElement("div");
+        carta.className="items col-12 col-md-6 col-lg-3";
+        carta.innerHTML=`<div>
+        <img class="img-fluid mt-4" src="../imagenes/todos los productos/${prod.imagen}.jpg" alt="">
+        <h3>${prod.nombre}</h3>
+        <p>Precio: $${prod.precio}</p>
+        <p>Stock: ${prod.stock}</p>
+        <button id="btn1" class="btn btn-warning">Comprar</button>
+    </div>
+        `;
+        cartas.append(carta);
+    }
+}
+
+crearTarjeta()
+
+let botonComprar = document.getElementById("btn1");    
     
     
 //-SALUDO-
@@ -35,7 +58,7 @@ function saludar() {
 saludar ();
 
 //-CONSULTA PRODUCTOS-
-let seleccionarProd = prompt ("Ingresa un producto:\nbanco niza\nmesa londres\nmueble york\nmueble mini york\nmesa zagreb\nmesa panama\nperchero gales\nperchero lyon");
+let seleccionarProd = prompt("Ingresa un producto:\nbanco niza\nmesa londres\nmueble york\nmueble mini york\nmesa zagreb\nmesa panama\nperchero gales\nperchero lyon");
 let carrito=[];
 while(seleccionarProd != "finalizar"){
     let prodElegido = productos.find((prod) => prod.nombre == seleccionarProd);
@@ -53,29 +76,47 @@ while(seleccionarProd != "finalizar"){
 const total = carrito.reduce((acc,prod) => acc+prod.precio,0);
 alert("Total de la suma $"+total)
 console.log("Total de la suma $"+total)
-
-//-DESPEDIDA-
-function despedir() {
-    alert("Gracias por tu compra!!")
-}
-despedir()    
+    
 
 //MAP-(se pasan a mayúsculas los nombres)
 const actualizado = productos.map((prod) => {
     return {
-    nombre: prod.nombre.toUpperCase(),
+    nombre: prod.nombre.toLowerCase(),
     precio: prod.precio 
     }
 })
 console.table(actualizado);
 
-//FIND-(se busca el código "m04")
-const busqueda = productos.find((prod) => prod.codigo === "m04");
-console.log(busqueda);
+//FIND-(buscador de productos por código)
+function buscador(){
+    let consultaCodigo=prompt("Ingresa el código del producto para saber si hay stock");
+    const busqueda = productos.find((prod) => prod.codigo === consultaCodigo);
+    if(busqueda != undefined){
+        alert("Hay stock de: "+busqueda.nombre);
+        console.log(busqueda);
+    }else{
+        alert("No hay stock");
+        consultaCodigo = prompt("Ingresa el código del producto");
+    }
+}
 
-//FILTER-(se filtran los productos menores a $7000)
-const filtroPrecio = productos.filter((prod) => prod.precio < 7000);
-console.table(filtroPrecio);
+buscador();
+
+//FILTER-(se filtran los productos menores a precio ingresado)
+function filtrar(){
+    let filtrarBaratos = parseInt(prompt("Ingresa un precio para ver los más baratos"));
+    const filtroPrecio = productos.filter((prod) => prod.precio < filtrarBaratos );
+    console.table(filtroPrecio);
+}
+
+filtrar();
+
+//-DESPEDIDA-
+function despedir() {
+    alert("Gracias por tu compra!!")
+}
+despedir()
+
 
 
 
